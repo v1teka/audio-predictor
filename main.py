de32eebaaee22d4a3ac5e9390fdb21071ceb3024
Victor Kozlovsky <viktor.kozlovsky@x5.ru>
2022-04-21 19:07:47 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# for each song name in /input/ folder
+    # find in spotify -> Song ID
+    # fetch metadata
+    # extract WAV file
+    # create histogram
+    # extract content features (pooling)
+    # neural network predict
+    # print result interpretation
