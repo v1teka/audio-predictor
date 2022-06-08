3788ca8e3aced86f62a6737b63769e4de26c49df
Victor Kozlovsky <viktor.kozlovsky@x5.ru>
2022-06-08 15:10:50 +0300

    remove unused files

--- main.py
+++
@@ -1,9 +0,0 @@
-
-# for each song name in /input/ folder
-    # find in spotify -> Song ID
-    # fetch metadata
-    # extract WAV file
-    # create histogram
-    # extract content features (pooling)
-    # neural network predict
-    # print result interpretation
